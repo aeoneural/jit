851b766722c3a8f46a03e8d8bb5b68073bd87bd4
aikerim <aygerim.sauletkhan@gmail.com>
2018-05-15 22:45:45 -0700

    added full description of the problem

--- practice solved problems /nextGreaterElements.js
+++ practice solved problems /nextGreaterElements.js
@@ -1,3 +1,9 @@
+/* 
+
+Problem: Given a circular array (the next element of the last element is the first element of the array), print the Next Greater Number for every element. The Next Greater Number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, output -1 for this number.
+
+*/
+
 var nextGreaterElements = function(nums) { 
     if (nums.length === 0) return [];
 
