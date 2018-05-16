714f2abf6c36e5c0d3756204cf6a4770db4e2f01
aikerim <aygerim.sauletkhan@gmail.com>
2018-05-16 12:31:03 -0700

    problem description

---
+++ practice solved problems /findDuplicate.js
@@ -0,0 +1,9 @@
+/* 
+
+problem: given an array nums containing n + 1 integers where each int is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one 
+
+you must not modify the array 
+use only constant space 
+runtime < O(n^2)
+
+*/
