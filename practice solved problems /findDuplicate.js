f7d7f92e1dcaa7f97f9cc348f533c89b35a5037b
aikerim <aygerim.sauletkhan@gmail.com>
2018-05-16 12:40:35 -0700

    two strategies to solve the problem O(nlogn), O(n)

--- practice solved problems /findDuplicate.js
+++ practice solved problems /findDuplicate.js
@@ -6,4 +6,24 @@ you must not modify the array
 use only constant space 
 runtime < O(n^2)
 
+strategies: 
+1. Binary Search 
+    for each selected number (mid), count all the numbers equal to or less than mid 
+        if count >  number, search space will be [1, mid]
+        else [mid + 1, n]
+    includes pigeon hole principle 
+2. Linked list cycle detection method 
+    substract 1 from each element in the array
+    choose the last element as the head of linked list 
+    example: 
+        index: 0 1 2 3 4 5 
+        value: 2 5 1 1 4 3 
+        -- substract 1 from values 
+        index: 0 1 2 3 4 5
+        value: 1 4 0 0 3 2
+
+        5 -> 2 -> 0 <- 3 
+                  |    | cycle here 
+                  ˇ    |
+                  4 -> 3 
 */
