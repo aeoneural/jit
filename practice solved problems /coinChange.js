faf4ad89fd3f7abeea7f852d5e17d7013ea35fdf
aikerim <aygerim.sauletkhan@gmail.com>
2018-05-17 12:43:32 -0700

    implemented bottom-up algorithm to build up a memoization

--- practice solved problems /coinChange.js
+++ practice solved problems /coinChange.js
@@ -2,12 +2,27 @@ var coinChange = function(amount, coins) {
     var combinations = new Array(amount + 1).fill(0); 
     combinations[0] = 1; 
     for (var coin = 0; coin < coins.length; coin++) {
-        for (var i = 1; i < combinations.length; i++) {
-            if (i >= coins[coin]) { 
-                combinations[i] += combinations[i - coins[coin]];
-            }
+        for (var i = coins[coin]; i < combinations.length; i++) {
+            combinations[i] += combinations[i - coins[coin]]
         } 
     }
     return combinations[amount];
 }
-console.log(coinChange(5, [1, 2, 5]))
+
+var coinChangeRecursive = function(amount, coins) { 
+    var waysOfDoingNCoins = [];
+
+    for (var i = 0; i <= amount; i++) { 
+        waysOfDoingNCoins[i] = 0;
+    }
+    waysOfDoingNCoins[0] = 1; 
+
+    coins.forEach((coin)=>{ 
+        for (var higherAmount = coin; higherAmount <= amount; higherAmount++) { 
+            var remainder = higherAmount - coin; 
+            waysOfDoingNCoins[higherAmount] += waysOfDoingNCoins[remainder];
+        }
+    })
+    return waysOfDoingNCoins[amount];
+}
+console.log(coinChangeRecursive(5, [1, 2, 5]))
